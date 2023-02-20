f43d89bdc44c109e29284201bbb48facc16da117
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-20 18:11:14 +0800

    Set up firestore db and react-router-dom

--- my-app/src/firebase-config.js
+++ my-app/src/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
 import {getAuth,GoogleAuthProvider} from 'firebase/auth'; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,6 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 
