72bf22ffc64c37673dc37f477ce19e688fcc6675
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-20 17:58:13 +0800

    Setting up react app and firebase

---
+++ my-app/src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDYxtegUeFJGhnHDWjPic56xzu24107-M",
+  authDomain: "farmknowledge-7f477.firebaseapp.com",
+  projectId: "farmknowledge-7f477",
+  storageBucket: "farmknowledge-7f477.appspot.com",
+  messagingSenderId: "861632834719",
+  appId: "1:861632834719:web:1ffad21066bf27e80046aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
