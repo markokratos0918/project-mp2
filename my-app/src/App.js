709130e845575d3a3e7c775ed072933d7a721eb2
markokratos0918 <117650809+markokratos0918@users.noreply.github.com>
2023-02-20 18:37:25 +0800

    Added Routes for pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
     <Router>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/createpost" element={<CreatePost/>} />
+        <Route path="/Login" element={<Login/>} />
+      </Routes>
     </Router>
   )
 }
